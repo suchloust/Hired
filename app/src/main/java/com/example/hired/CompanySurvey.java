7a0fb461777e83848c4a8662aab7211eada969a7
kiaralavana <klavana05@gmail.com>
2022-04-05 09:14:35 -0400

    Commiting the creation of activity_profile, activity_company_survey, activity_user_survey.xml, and addition of User/Company classes

--- app/src/main/java/com/example/hired/CompanySurvey.java
+++ app/src/main/java/com/example/hired/CompanySurvey.java
@@ -3,13 +3,12 @@ package com.example.hired;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
-public class activity_profile extends AppCompatActivity {
+public class CompanySurvey extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_company_survey);
     }
 }
