b8e8e5aacf6fcccb7c22cc1b8be14d2706ea3100
kiaralavana <klavana05@gmail.com>
2022-04-05 21:35:30 -0400

    Commiting the creation of activity_profile, activity_company_survey, activity_user_survey.xml, and addition of User/Company classes

--- app/src/main/java/com/example/hired/CompanySurvey.java
+++ app/src/main/java/com/example/hired/CompanySurvey.java
@@ -3,12 +3,22 @@ package com.example.hired;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CompanySurvey extends AppCompatActivity {
+
+    private Question surveyQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_survey);
+
+        final Controller aController = (Controller) getApplicationContext();
+
+        //displays the question from the ArrayList of questions in the questionView text box on the screen.
+        surveyQuestion = aController.getQuestion();
+        TextView companyTextView = findViewById(R.id.questionView);
+        companyTextView.setText(surveyQuestion.getQuestion());
     }
 }
--- app/src/main/java/com/example/hired/MainActivity.java
+++ app/src/main/java/com/example/hired/MainActivity.java
@@ -11,6 +11,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -40,5 +45,42 @@ public class MainActivity extends AppCompatActivity {
                 Log.w("MainActivity", "Failed to read value.", error.toException());
             }
         });
+
     }
+    //method to read Company questions and make them instances of Question class. Called only once.
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        readQuestionData();
+    }
+
+    private void readQuestionData(){
+        InputStream input = getResources().openRawResource(R.raw.companyquestions);
+
+        //allows the file to be read one line at a time:
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+        //Get the Global Controller Class object
+        final Controller aController = (Controller) getApplicationContext();
+
+        String line = "";
+        try {
+            while ((line = reader.readLine()) != null){
+                //Split by comma ",". Use if there are commas separating the line.
+               // String[] fields = line.split(",");
+
+                //Log.v("MainActivity",fields[0] + " " + fields[1]);
+
+                //adds the question from the file to the ArrayList of company Questions.
+                Question compQ = new Question(line);
+                aController.addQuestion(compQ);
+            }
+        }
+        catch(IOException e){
+            Log.wtf("MainActivity","Error reading data on line: " + line);
+
+        }
+    }
+
 }
