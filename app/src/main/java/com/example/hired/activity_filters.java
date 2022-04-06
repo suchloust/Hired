acdb0ae99632363bbd6e10c62300823b8a7444eb
suchloust <diegosuchenskiloustaunau@gmai.com>
2022-04-06 01:42:55 -0400

    Working on search bar

--- app/src/main/java/com/example/hired/activity_filters.java
+++ app/src/main/java/com/example/hired/activity_filters.java
@@ -1,6 +1,27 @@
 package com.example.hired;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_filters extends AppCompatActivity {
-}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filters);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+
+    private void doMySearch(String query) {
+        /*eventually we need to add our database that gets searched here*/
+    }
+    }
+
