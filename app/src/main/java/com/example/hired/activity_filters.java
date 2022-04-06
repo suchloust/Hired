cf2f5a32bcc4aa1a085bacd813e98548fcf498eb
suchloust <diegosuchenskiloustaunau@gmai.com>
2022-04-06 02:08:05 -0400

    Working on search bar

--- app/src/main/java/com/example/hired/activity_filters.java
+++ app/src/main/java/com/example/hired/activity_filters.java
@@ -1,10 +1,14 @@
 package com.example.hired;
 
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 public class activity_filters extends AppCompatActivity {
     @Override
@@ -23,5 +27,20 @@ public class activity_filters extends AppCompatActivity {
     private void doMySearch(String query) {
         /*eventually we need to add our database that gets searched here*/
     }
-    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        return true;
+    }}
 
