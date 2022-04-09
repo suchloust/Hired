cc2f2cc5dd51a4730902d320757d8e4d4d81cb74
suchloust <diegosuchenskiloustaunau@gmai.com>
2022-04-09 12:11:41 -0400

    Adding video functionality

--- app/src/main/java/com/example/hired/ProfileActivity.java
+++ app/src/main/java/com/example/hired/ProfileActivity.java
@@ -1,20 +1,18 @@
 package com.example.hired;
 
 import static com.example.hired.userSurvey.getPrefs;
-import static com.example.hired.userSurvey.setPrefs;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
     Button filtersBut;
+    Button videosBut;
     TextView locationText;
 
 
@@ -23,12 +21,21 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         filtersBut = (Button) findViewById(R.id.filtersButton);
+        videosBut = (Button) findViewById(R.id.videos_button);
         locationText = (TextView) findViewById(R.id.locationLabel);
         locationText.setText(getPrefs(this));
         filtersBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), userSurvey.class);
+                startActivity(intent);
+            }
+
+        });
+        videosBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MultimediaPlayer.class);
                 startActivity(intent);
             }
 
