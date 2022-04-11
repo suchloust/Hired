a3ee784121ade8d2d84fd54e98c6f545905b9e8b
suchloust <diegosuchenskiloustaunau@gmai.com>
2022-04-11 08:04:37 -0400

    Adding video functionality

--- app/src/main/java/com/example/hired/MultimediaPlayer.java
+++ app/src/main/java/com/example/hired/MultimediaPlayer.java
@@ -37,19 +37,8 @@ public class MultimediaPlayer extends Activity
                 startActivity(intent);
             }});
 
-        //Hard coded right now but eventually should link with url company enters
-       // String media_url = "www.massacademy.org";
 
         mWebView = (WebView) findViewById(R.id.webviewer);
-        /*mWebView.setWebChromeClient(new WebChromeClient());
-
-        WebSettings ws = mWebView.getSettings();
-        ws.setBuiltInZoomControls(true);
-        ws.setJavaScriptEnabled(true);
-
-        mIsPaused = true;
-        resumeBrowser();
-        mWebView.loadUrl(media_url);*/
 
         final WebView webview = (WebView) findViewById(R.id.webviewer);
         webview.setWebViewClient(new WebViewClient());
@@ -57,6 +46,8 @@ public class MultimediaPlayer extends Activity
         WebSettings websetting = webview.getSettings();
         websetting.setJavaScriptEnabled(true);
         websetting.setDomStorageEnabled(true);
+        mIsPaused = true;
+        resumeBrowser();
         webview.loadUrl("https://youtube.com/watch?v=R1HW6Comeno");
     }
 
